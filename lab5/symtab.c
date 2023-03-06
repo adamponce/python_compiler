279d6bee7e8cae02c3c48c71e6bee4cee8abcf4f
nikki-sparacino <56003779+nikki-sparacino@users.noreply.github.com>
2023-03-06 16:01:07 -0700

    changes for lab 5 to print NAME symbols

--- lab5/symtab.c
+++ lab5/symtab.c
@@ -1,9 +1,20 @@
+/**
+ * symtab.c
+ * Javier Reyna; Adam Schmidt; Nikki Sparacino
+ * CSE423 Lab 5 
+ * 
+ * This program builds a symbol table for our Python compiler.
+ */
 #include <stdio.h>
-// #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 #include "symtab.h"
 #include "tree.h"
 #include "punygram.tab.h"
 
+char *alloc(int n);
+
+/* compute the hash value */
 int hash(SymbolTable st, char *s){
    register int h = 0;
    register char c;
@@ -15,40 +26,44 @@ int hash(SymbolTable st, char *s){
    return h % st->nBuckets;
 }
 
+/* initialize a new symbol table*/
 SymbolTable new_st(int nbuckets) {
-    int h;
+    // int h; /* h not used */
     SymbolTable rv;
-    rv = (SymbolTable) alloc(sizeof(struct sym_table));
+    rv = (SymbolTable) alloc((unsigned int) (sizeof(struct sym_table)));
     rv->tbl = (struct sym_entry **) alloc((unsigned int) (nbuckets * sizeof(struct sym_entry *)));
     rv->nBuckets = nbuckets;
     rv->nEntries = 0;
     return rv;
 }
 
+/* traverse the tree and call the printsymbol function for each node */
 void printsyms(struct tree *t){
     // tree traversal to call printsymbol function
-    int i;
-    printsymbol(t->symbolname);
-    for(i = 0; i < t->nkids; i++){
-        // treeprint(t->kids[i], depth+1);
-        printsyms(t->kids[i]);
-    }
+   int i;
+   if(t == NULL) return;
+   if(t->nkids > 0) {
+      for(i = 0; i < t->nkids; i++){
+         printsyms(t->kids[i]);
+      }
+   } else if (t->leaf) {
+      if(t->leaf->category == NAME) {
+         printsymbol(t->symbolname);
+      }
+   }  
 }
 
-/* Jeffery's version -- gives seg fault */
-// void printsyms(struct tree *t)
-// {
-//     if (t == NULL) return;
-//     if (t->nkids > 0) {
-//         int i;
-//         for(i=0;i<t->nkids; i++) printsyms(t->kids[i]);
-//     }
-//     /* else you are a leaf, but are you an identifier? */
-//     else {
-//         if(t->leaf->category == NAME) printsymbol(t->leaf->text);
-//     }
-// }
+/* error check calloc for NULL */
+char *alloc(int n) {
+   char *a = calloc(n, sizeof(char));
+   if (a == NULL) {
+      fprintf(stderr, "alloc(%d): out of memory\n", (int)n);
+      exit(-1);
+      }
+   return a;
+}
 
+/* print the given symbol, s */
 void printsymbol(char *s)
 {
    printf("%s\n", s); fflush(stdout);
--- lab5/symtab.h
+++ lab5/symtab.h
@@ -1,12 +1,5 @@
 #include "tree.h"
 
-typedef struct sym_entry {
-//   SymbolTable table;			/* what symbol table do we belong to*/
-   char *s;				/* string */
-   /* more symbol attributes go here for code generation */
-   struct sym_entry *next;
-   } *SymbolTableEntry;
-
 /*Symbol table that uses an array of linked lists*/
 typedef struct sym_table {
    int nBuckets;			/* # of buckets */
@@ -15,6 +8,13 @@ typedef struct sym_table {
    struct sym_entry **tbl;
    /* more per-scope/per-symbol-table attributes go here */
    } *SymbolTable;
+
+typedef struct sym_entry {
+//   SymbolTable table;			/* what symbol table do we belong to*/
+   char *s;				/* string */
+   /* more symbol attributes go here for code generation */
+   struct sym_entry *next;
+   } *SymbolTableEntry;
 
 int hash(SymbolTable st, char *s);
 void printsyms(struct tree *t);
