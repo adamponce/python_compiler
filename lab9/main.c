94a5074e8a7ea42d824d33a234873d183663cca2
javiereyna10 <javiereyna10@gmail.com>
2023-04-16 20:59:43 -0600

    lab9 submittion

--- lab9/main.c
+++ lab9/main.c
@@ -1,4 +1,15 @@
+/**
+* @file main.c
+*
+* @author Javier Reyna Adam Schmidt Nikki Sparacino
+*
+* @date 04/16/2023
+*
+* Assignment: lab 9
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "tac.h"
 #include <string.h>
 
@@ -7,8 +18,6 @@ int main(int argc, char *argv[]){
 	struct addr cont_5 = {R_CONST, {5}};
 	struct addr none_addr = {R_NONE, {0}};
 	struct instr *tac = gen(O_ASN, loc_0, cont_5, none_addr);
-
-
 	struct addr loc_8 = {R_LOCAL, {8}};
 	struct instr *mult = gen(O_MUL, loc_8, loc_0, loc_0);
 	tac = append(tac, mult);
@@ -27,33 +36,33 @@ int main(int argc, char *argv[]){
 	struct instr *parameter = gen(O_PARM, loc_0, none_addr, none_addr);
 	tac = append(tac, parameter);
 
-	struct addr to_string = {R_NAME};
-	to_string.u.name = "tostr";
+	struct addr string = {R_NAME};
+	string.u.name = "tostr";
 	struct addr loc_24 = {R_LOCAL, {24}};
-	struct instr *call = gen(O_CALL, to_string, loc_24, none_addr);
+	struct instr *call = gen(O_CALL, string, loc_24, none_addr);
 	tac = append(tac, call);
 
-	struct addr str_0 = {R_NAME};
-	str_0.u.name = "str";
-	parameter = gen(O_PARM, str_0, none_addr, none_addr);
+	struct addr str = {R_NAME};
+	str.u.name = "str";
+	parameter = gen(O_PARM, str, none_addr, none_addr);
 	tac = append(tac, parameter);
 
 	parameter = gen(O_PARM, loc_24, none_addr, none_addr);
 	tac = append(tac, parameter);
 
-	struct addr cat_2 = {R_NAME};
-	cat_2.u.name = "cat";
+	struct addr cat = {R_NAME};
+	cat.u.name = "cat";
 	struct addr loc_32 = {R_LOCAL, {32}};
-	call = gen(O_CALL, cat_2, loc_32, none_addr);
+	call = gen(O_CALL, cat, loc_32, none_addr);
 	tac = append(tac, call);
 
 	parameter = gen(O_PARM, loc_32, none_addr, none_addr);
 	tac = append(tac, parameter);
 
-	struct addr println_1 = {R_NAME};
-	println_1.u.name = "println";
+	struct addr println = {R_NAME};
+	println.u.name = "println";
 	struct addr loc_40 = {R_LOCAL, {40}};
-	call = gen(O_CALL, println_1, loc_40, none_addr);
+	call = gen(O_CALL, println, loc_40, none_addr);
 	tac = append(tac, call);
 
 	struct instr *ret = gen(O_RET, none_addr, none_addr, none_addr);
